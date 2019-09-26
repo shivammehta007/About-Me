0a4e07530a8f6db4849726a8dea0aaf0efabae02
Shivam Mehta <shivam.mehta007@gmail.com>
2019-09-26 19:04:03 +0300

    Adding Button to show and hide some text

--- app/src/main/java/me/shivammehta/learning/aboutme/MainActivity.kt
+++ app/src/main/java/me/shivammehta/learning/aboutme/MainActivity.kt
@@ -1,12 +1,36 @@
 package me.shivammehta.learning.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editText : EditText
+    lateinit var nicknameTextView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            editNickName(it)
+        }
+    }
+    private fun editNickName(view : View){
+        editText = findViewById(R.id.nickname_edit)
+        nicknameTextView = findViewById(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
